1b09a1ac986f0bde8567e07d5a8b8fe9120149be
Aditya Tripathi <adityatripathi.at04@gmail.com>
2022-08-08 11:27:10 +0530

    modded

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ require("dotenv").config();
 const app = express();
 
 mongoose
-  .connect(process.env.DB)
-  .then(() => console.log("DB CONNECTED"))
-  .catch((err) => console.log(err));
+	.connect(process.env.DB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB CONNECTED"))
+	.catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
 fs.readdirSync("./routes").map((route) => {
-  app.use("/api", require(`./routes/${route}`));
+	app.use("/api", require(`./routes/${route}`));
 });
 
 app.listen(process.env.PORT, () => console.log("SERVER STARTED"));
