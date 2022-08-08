4a6ff19a046565853242b8442bb947bf41f93b0f
Aditya Tripathi <adityatripathi.at04@gmail.com>
2022-08-08 21:09:43 +0530

    changed

--- routes/report.js
+++ routes/report.js
@@ -3,7 +3,6 @@ const {
 	addReport,
 	getReports,
 	getOneReport,
-	moderateRep,
 } = require("../controllers/report");
 
 const router = express.Router();
@@ -11,6 +10,5 @@ const router = express.Router();
 router.post("/report", addReport);
 router.get("/r/:id", getOneReport);
 router.get("/reports", getReports);
-router.post("/moderate-report", moderateRep);
 
 module.exports = router;
