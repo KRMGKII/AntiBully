63b26538d02f4ebbb2f9a0e59376368d7b30b7aa
Aditya Tripathi <adityatripathi.at04@gmail.com>
2022-08-09 00:22:35 +0530

    FINAL

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { ReportsComponent } from './pages/reports/reports.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'articles', component: ArticlesComponent },
+  { path: '', component: ReportsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,32 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  getArticles() {
+    return this.http.get(`${environment.api}all-articles`);
+  }
+
+  getReports() {
+    return this.http.get(`${environment.api}reports`);
+  }
+
+  removeArticle(id: String) {
+    return this.http.post(`${environment.api}delete-article`, { id });
+  }
+
+  removeReports(id: String) {
+    return this.http.post(`${environment.api}delete-report`, { id });
+  }
+
+  acceptArticle(id: String) {
+    return this.http.post(`${environment.api}accept-article`, {
+      id,
+    });
+  }
+}
